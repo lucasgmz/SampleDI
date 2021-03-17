c642a13d680fc78642fb6d4637c46e04a2095108
dvGomez <gomezprojetos@gmail.com>
2021-03-17 19:52:27 -0300

    Comments addedd

--- SampleDI.WebApi/Startup.DI.cs
+++ SampleDI.WebApi/Startup.DI.cs
@@ -12,6 +12,10 @@ namespace SampleDI.WebApi
     {
         public static void RegisterServices(this IServiceCollection services)
         {
+            // MathUtilsService is being used just to make a reference
+            // Basically, this code says: Hey DOT.NET, scan all classes from that assembly and register them on each scope
+            // With their public interfaces
+            // =)
             services.RegisterAssemblyPublicNonGenericClasses(typeof(MathUtilsService).Assembly)
                 .Where(x => x.Name.EndsWith("Service"))
                 .AsPublicImplementedInterfaces(ServiceLifetime.Scoped);
